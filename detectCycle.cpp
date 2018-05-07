b35f0bc1579ff3a3e6c847b563d56a543f8c0499
Pradeep Patel <pradpate@cisco.com>
2018-05-06 18:42:51 -0700

    Given a linked list, determine if it has a cycle in it

---
+++ detectCycle.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std; 
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow =head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast) return true; 
+        }
+        return false;
+    }
+};
+
+int main()
+{
+   return 1;
+}
